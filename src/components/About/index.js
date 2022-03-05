efa2eb5ccb4e56353ae1e6b1fa3cfac2aa0f0282
Austin Gilmore <austingilmora@gmail.com>
2022-03-05 16:40:08 -0500

    added initial files and construct

---
+++ src/components/About/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import mypic from "../../assets/images/me.jpg";
+
+function About() {
+    return (
+        <section>
+            <h1 id="about">About Me</h1>
+            <img src={mypic} className="cover-pic" alt="cover" />
+            <div class="about-me">
+            <p class="hello-world">
+                Hi, I'm Austin.
+            </p>
+            <p className="my-2 flex-row">
+                I'm a web developer. Originally from the US, I have been making my way all around the world for the past 5 or 6 years. 
+                I've lived in 5 different countries and many more places inside of those countries. Living all around the world has definitely
+                been helped by my interest in learning new languages, speaking 4 different langauges at intermediate to fluent levels.
+                Now, I'm turning that affinity for learning new languages onto coding.
+            </p>
+            <p className="my-2 flex-row">
+                I’m an autonomous self-starter who is well-accustomed to working independently and remotely. Through my years of experience 
+                teaching ESL, I have gained planning and project management skills, through creating and adjusting the curriculum to suit student 
+                groups. I have also gained excellent written and verbal communication skills through learning to communicate effectively across language barriers. 
+            </p>
+        </div>
+        </section>
+    );
+}
+
+export default About;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+
+function Nav(props) {
+
+
+    return (
+        <header className="flex-row px-1">
+            <h2>
+                <a href="/">
+                    AG
+                </a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    <li className="mx-2">
+                        
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Nav;
